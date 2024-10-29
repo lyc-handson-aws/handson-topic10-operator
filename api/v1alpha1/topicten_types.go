08aad05fed89958ed873fc1bf64d075480729c67
Yuanchao LI <yuanchao.li@qq.com>
2024-10-29 22:03:32 +0100

    init push

---
+++ api/v1alpha1/topicten_types.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// TopicTenSpec defines the desired state of TopicTen
+type TopicTenSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// +kubebuilder:validation:Minimum=5
+	// +kubebuilder:validation:Maximum=10
+	// +kubebuilder:validation:Required
+	Replicas int32 `json:"replicas"`
+
+	// +kubebuilder:validation:Required
+	Image string `json:"image"`
+}
+
+// TopicTenStatus defines the observed state of TopicTen
+type TopicTenStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	CurrentReplicas int32 `json:"currentReplicas"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// TopicTen is the Schema for the topictens API
+type TopicTen struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TopicTenSpec   `json:"spec,omitempty"`
+	Status TopicTenStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// TopicTenList contains a list of TopicTen
+type TopicTenList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []TopicTen `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&TopicTen{}, &TopicTenList{})
+}
---
+++ internal/controller/topicten_controller.go
@@ -0,0 +1,134 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	appv1alpha1 "github.com/lyc-handson-aws/handson-topic10-operator/api/v1alpha1"
+)
+
+// TopicTenReconciler reconciles a TopicTen object
+type TopicTenReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=app.lyc-handson-aws.com,resources=topictens,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=app.lyc-handson-aws.com,resources=topictens/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=app.lyc-handson-aws.com,resources=topictens/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the TopicTen object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
+func (r *TopicTenReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+
+	log := log.FromContext(ctx)
+	log.Info("starting reconciliation - TopicTen")
+
+	topicTen := &appv1alpha1.TopicTen{}
+
+	// Fetch the TopicTen instance
+	if err := r.Get(ctx, req.NamespacedName, topicTen); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Define the desired Deployment object
+	desiredDeployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      topicTen.Name,
+			Namespace: topicTen.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &topicTen.Spec.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"app": topicTen.Name},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"app": topicTen.Name},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "topicten-container",
+							Image: topicTen.Spec.Image, // Use the specified image
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Set TopicTen instance as the owner of the Deployment
+	if err := controllerutil.SetControllerReference(topicTen, desiredDeployment, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Try to fetch the existing Deployment
+	foundDeployment := &appsv1.Deployment{}
+	err := r.Get(ctx, req.NamespacedName, foundDeployment)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Info("Creating Deployment", "Namespace", req.Namespace, "Name", req.Name)
+			if err := r.Create(ctx, desiredDeployment); err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{RequeueAfter: 2 * time.Minute}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Reduce replica count every 2 minutes if greater than 1
+	if *foundDeployment.Spec.Replicas > 1 {
+		*foundDeployment.Spec.Replicas--
+		if err := r.Update(ctx, foundDeployment); err != nil {
+			return ctrl.Result{}, err
+		}
+		log.Info(fmt.Sprintf("Reduced replicas to %d", *foundDeployment.Spec.Replicas))
+		return ctrl.Result{RequeueAfter: 2 * time.Minute}, nil
+	}
+
+	log.Info("end of reconciliation - TopicTen")
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *TopicTenReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&appv1alpha1.TopicTen{}).
+		Complete(r)
+}
